2b9d25a6404356388230d93587e9c2bd17cab243
MnuelD <manueljosedala@yahoo.com>
2025-10-07 20:04:12 +0200

    INSTALL: installl dependenies

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::post('/logout', function (Request $request) {
     Auth::logout(); // faz logout
     $request->session()->invalidate(); // invalida a sessão
     $request->session()->regenerateToken();
-    return redirect('/login'); // redireciona para página inicial
+    return redirect('/login'); // redireciona para página iniciall
 })->name('logout');
 Route::get('/verify-email/{token}', [AuthController::class, 'verifyEmail'])->name('verify.email');
 Route::post('/verify-sms', [AuthController::class, 'verifySms'])->name('verify.sms');
